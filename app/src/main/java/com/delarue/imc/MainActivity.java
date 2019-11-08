b243fb9d2b2c74765c4c342f281940ad33e92cc0
J4R84S <jarbasstm@hotmail.com>
2019-11-07 22:22:02 -0300

    Atualizado 07112019

--- app/src/main/java/com/delarue/imc/MainActivity.java
+++ app/src/main/java/com/delarue/imc/MainActivity.java
@@ -3,12 +3,90 @@ package com.delarue.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaração de vaiaveis
+
+    float peso;
+    float altura;
+    String mensagem = "";
+    float imc;
+
+
+    // Declaração de Objetos
+
+    EditText editPeso, editAltura;
+    TextView txtResultado, txtDiagnostico;
+    Button btnCalcular;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setar (Apontar) os Objetos
+
+        editPeso = (EditText) findViewById(R.id.editPeso);
+        editAltura = (EditText) findViewById(R.id.editAltura);
+        txtResultado = (TextView) findViewById(R.id.txtResultado);
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Recuperar os valores digitados
+
+                peso = Float.parseFloat(editPeso.getText().toString());
+                altura = Float.parseFloat(editAltura.getText().toString());
+
+                imc = peso / (altura * altura);
+
+                // Validação da IMC
+
+                if (imc < 17){
+                    mensagem = "Muito Abaixo Do Peso";
+
+                }else if ((imc >=17)&& (imc < 18.5)){
+
+                    mensagem = "Abaixo Do Peso";
+
+                }else if ((imc >=18.5)&& (imc < 25)){
+
+                    mensagem = "Peso Ideal";
+
+                }else if ((imc >=25)&& (imc < 30)) {
+
+                    mensagem = "Acima Do Peso";
+
+                }else if ((imc >=30)&& (imc < 35)) {
+
+                    mensagem = "Obesidade Grau I";
+
+                }else if ((imc >=35)&& (imc < 40)) {
+
+                    mensagem = "Obsidade Grau II";
+
+                }else {
+
+                    mensagem = "Obesidade Mórbida";
+
+                }
+
+                // Mostrar o resultado
+
+                txtResultado.setText(String.valueOf(imc));
+                txtDiagnostico.setText(mensagem);
+
+            }
+
+                });
     }
 }
