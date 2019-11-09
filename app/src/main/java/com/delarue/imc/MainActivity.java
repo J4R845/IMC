8a3cb9bac2b210d074e88711642d02c0393374d6
J4R84S <jarbasstm@hotmail.com>
2019-11-09 17:46:45 -0300

    Atualizado 09112019

--- app/src/main/java/com/delarue/imc/MainActivity.java
+++ app/src/main/java/com/delarue/imc/MainActivity.java
@@ -3,30 +3,34 @@ package com.delarue.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static java.lang.String.format;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declaração de vaiaveis
+    // Declaração de variaveis
 
     float peso;
     float altura;
     float imc;
     String mensagem = "";
 
+    boolean dadosValidados;
 
 
     // Declaração de Objetos
 
     EditText editPeso, editAltura;
     TextView txtResultado, txtDiagnostico;
-    Button btnCalcular;
+    Button btnCalcular, btnSalvar;
+
 
 
     @Override
@@ -41,60 +45,116 @@ public class MainActivity extends AppCompatActivity {
         txtResultado =  findViewById(R.id.txtResultado);
         txtDiagnostico =  findViewById(R.id.txtDiagnostico);
         btnCalcular =  findViewById(R.id.btnCalcular);
+        btnSalvar =  findViewById(R.id.btnSalvar);
+
 
 
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                try {
+
+                    txtResultado.setText("");
+                    txtDiagnostico.setText("");
+
+                    dadosValidados = validarCampos();
+
+                    // Recuperar os valores digitados
 
 
-                // Recuperar os valores digitados
+                    if (dadosValidados) {
 
-                peso = Float.parseFloat(editPeso.getText().toString());
-                altura = Float.parseFloat(editAltura.getText().toString());
+                        peso = Float.parseFloat(editPeso.getText().toString());
+                        altura = Float.parseFloat(editAltura.getText().toString());
 
-                imc = peso / (altura * altura);
+                        imc = peso / (altura * altura);
 
-                // Validação da IMC
+                        // Validação da IMC
 
-                if (imc < 17){
-                    mensagem = "Muito Abaixo Do Peso";
+                        if (imc < 17) {
+                            mensagem = "Muito Abaixo Do Peso";
 
-                }else if ((imc >= 17) && (imc < 18.5)){
+                        } else if ((imc >= 17) && (imc < 18.5)) {
 
-                    mensagem = "Abaixo Do Peso";
+                            mensagem = "Abaixo Do Peso";
 
-                }else if ((imc >= 18.5) && (imc < 25)){
+                        } else if ((imc >= 18.5) && (imc < 25)) {
 
-                    mensagem = "Peso Ideal";
+                            mensagem = "Peso Ideal";
 
-                }else if ((imc >= 25) && (imc < 30)) {
+                        } else if ((imc >= 25) && (imc < 30)) {
 
-                    mensagem = "Acima Do Peso";
+                            mensagem = "Acima Do Peso";
 
-                }else if ((imc >= 30) && (imc < 35)) {
+                        } else if ((imc >= 30) && (imc < 35)) {
 
-                    mensagem = "Obesidade Grau I";
+                            mensagem = "Obesidade Grau I";
 
-                }else if ((imc >= 35) && (imc < 40)) {
+                        } else if ((imc >= 35) && (imc < 40)) {
 
-                    mensagem = "Obesidade Grau II";
+                            mensagem = "Obesidade Grau II";
 
-                }else {
+                        } else {
 
-                    mensagem = "Obesidade Mórbida";
+                            mensagem = "Obesidade Mórbida";
+
+                        }
+
+                        // Mostrar o resultado
+
+                        txtResultado.setText(format("%.2f", imc));
+
+                        txtDiagnostico.setText(mensagem);
+
+                    }
+
+                }catch (Exception e){
+
 
                 }
 
-                // Mostrar o resultado
-
-                txtResultado.setText(format("%.2f",imc));
-
-                txtDiagnostico.setText(mensagem);
 
             }
-
         });
+
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getBaseContext(), "Nada Programado!!!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+
+    private boolean validarCampos(){
+
+        /* editPeso, editAltura; */
+
+        boolean retorno = false;
+
+        if(!TextUtils.isEmpty(editPeso.getText().toString())) {
+            retorno = true;
+        }else{
+            editPeso.setError("Digite O Peso!");
+            editPeso.requestFocus();
+        }
+
+            if (!TextUtils.isEmpty(editAltura.getText().toString())) {
+                retorno = true;
+            } else {
+                editAltura.setError("Digite A Altura!");
+                editAltura.requestFocus();
+            }
+
+
+        return retorno;
+    }
+
+
+
 }
